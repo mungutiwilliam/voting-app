cdb3b6819aa694174ca578d2137b9d39686be634
Caesar Mwangi <85876627+RudeNeighbour836@users.noreply.github.com>
2022-03-03 15:34:05 +0300

    updated

--- src/components/CardUI.jsx
+++ src/components/CardUI.jsx
@@ -6,18 +6,15 @@ const Card = (props) =>{
     return(
         <div className="card text-center">
             <div className="overflow">
-                <img className="card-img-top" src={ps4} alt="image 1"/>
             </div>
             <div className="card-body text-dark">
-                <h4 className="card-title">Card Title</h4>
+                <h4 className="card-title">Pick who you want to vote for.</h4>
                 <p className="card-text text-secondary">
-                    This will contain content to talk about the image on the card
                 </p>
-                <a href="#" className="btn btn-outline-secondary">Submit vote</a>
             </div>
         </div>
 
     );
 }
 
-export default Card
+export default Card;
