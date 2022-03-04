8bb5164e3b95d1e937d4cca56976f3e9b23350a6
William <mungutiwilliam97@gmail.com>
2022-03-04 21:57:27 +0300

    updates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,25 +7,25 @@ class card  extends Component {
     
     render() { 
         return (
-            <div className="container-fluid d-flex justify-content-center">
-                <div className="row">
+            <div id ="main" className=" bg-info container-fluid d-flex justify-content-center">
+                <div className="row text-white">
                     <div className="col-md-4">
-                        <Card name1="William" name2="Jack" name3="Loulou" title="President"/> 
+                        <Card nname1="William" nname2="Jack" nname3="Loulou" title="President"/> 
                     </div>
                     <div className="col-md-4">
-                        <Card name1="Jane" name2="Martha" name3="Maria" title="Vice President"/>
+                        <Card nname1="Jane" nname2="Martha" nname3="Maria" title="Vice President"/>
                     </div>
                     <div className="col-md-4">
-                        <Card name1="Brenda" name2="Caesar" name3="Peter" title="Secretary General"/>
+                        <Card nname1="Brenda" nname2="Caesar" nname3="Peter" title="Secretary General"/>
                     </div>
                     <div className="col-md-4">
-                        <Card name1="Noah" name2="Oliver" name3="Jean" title="Deputy Secretary General"/> 
+                        <Card nname1="Noah" nname2="Oliver" nname3="Jean" title="Deputy Secretary General"/> 
                     </div>
                     <div className="col-md-4">
-                        <Card name1="Liamson" name2="Joshua" name3="Tchalla" title=" Council Treasurer"/> 
+                        <Card nname1="Liamson" nname2="Joshua" nname3="Tchalla" title=" Council Treasurer"/> 
                     </div>
                     <div className="col-md-4">
-                        <Card name1="Alice" name2="Nzisa" name3="Peter" title=" Council Spokesperson"/> 
+                        <Card nname1="Alice" nname2="Nzisa" nname3="Peter" title=" Council Spokesperson"/> 
                     </div>
                 </div>
             </div> 
--- src/components/CardUI.jsx
+++ src/components/CardUI.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import './card-style.css';
 
-function countvotes(){
-    if(document.getElementById('candidate').checked) {   
-        var selectedValue = document.getElementById('candidate').value;  
-        alert("You have voted for: " + selectedValue);    
- }  
-
-
+const handlechange = event =>{
+    const target = event.target
+    const name = target.name
 }
 
 
@@ -15,18 +11,18 @@ function countvotes(){
 const Card = (props) =>{
 
     return(
-        <div className="card text-center">
+        <div className="bg-dark card text-center">
            <center>
                 <div className="list_names">   
-                                <input type="radio" id="candidate" name="Chairperson" value ={props.name1}></input>
-                                <label for="jane_doe">{props.name1}</label><br></br>
-                                <input type="radio" id="candidate" name="Chairperson" value={props.name2}></input>
-                                <label for="john_doe">{props.name2}</label><br></br>
-                                <input type="radio" id="candidate" name="Chairperson" value={props.name3}></input>
-                                <label for="stephen">{props.name3}</label><br></br>              
+                                <input on change ={handlechange}type="radio" id="candidate" name="Chairperson" value ={props.nname1}></input>
+                                <label for="jane_doe">{props.nname1}</label><br></br>
+                                <input type="radio" id="candidate2" name="Chairperson" value={props.nname2}></input>
+                                <label for="john_doe">{props.nname2}</label><br></br>
+                                <input type="radio" id="candidate3" name="Chairperson" value={props.nname3}></input>
+                                <label for="stephen">{props.nname3}</label><br></br>              
                 </div>
             </center>
-            <div className="card-body text-dark">
+            <div className="card-body text-white">
                 <h4 className="card-title">{props.title}</h4>
                 <p className="card-text text-danger">
                     Please Select One Candidate
@@ -36,8 +32,8 @@ const Card = (props) =>{
             <div className="overflow">
                 
             </div>
-            <div className="card-body text-dark">
-                <h4 className="card-title">Pick who you want to vote for.</h4>
+            <div className="card-body text-white">
+                <h4 className="card-title">Pick who to vote for</h4>
                 <p className="card-text text-secondary">
                 </p>
             </div>
