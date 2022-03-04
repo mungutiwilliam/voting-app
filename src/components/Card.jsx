1cedbcf139f014f9d5e4eb766ee605ee8fc8c21d
William <57761979+mungutiwilliam@users.noreply.github.com>
2022-03-04 19:47:15 +0300

    updated ui

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,107 +2,32 @@ import React, { Component } from "react";
 import Card from "./CardUI";
 
 
+
 class card  extends Component {
     
     render() { 
         return (
-            
             <div className="container-fluid d-flex justify-content-center">
-                
-                <form action="/action_page.php">
                 <div className="row">
-                
                     <div className="col-md-4">
-                        <h2>Chairperson</h2>
-                        <center>
-                            <div>
-                                <input type="radio" id="jane_doe" name="Chairperson" value="jane_doe"></input>
-                                <label for="jane_doe">Jane Doe</label><br></br>
-                                <input type="radio" id="john_doe" name="Chairperson" value="john_doe"></input>
-                                <label for="john_doe">John Doe</label><br></br>
-                                <input type="radio" id="stephen" name="Chairperson" value="Stephen"></input>
-                                <label for="stephen">Stephen</label><br></br>
-                                
-                            </div>
-                        </center>
-                        <Card/>
+                        <Card name1="William" name2="Jack" name3="Loulou" title="President"/> 
                     </div>
                     <div className="col-md-4">
-                        <h2>Vice Chairperson</h2>
-                            <center>
-                                <div>
-                                <input type="radio" id="1" name="Vice Chairperson" value="2"></input>
-                                <label for="1">Jane Doe</label><br></br>
-                                <input type="radio" id="3" name="Vice Chairperson" value="4"></input>
-                                <label for="3">John Doe</label><br></br>
-                                <input type="radio" id="5" name="Vice Chairperson" value="5"></input>
-                                <label for="5">Stephen</label><br></br>
-                                </div>
-                            </center>
-                            <Card/>
-                    </div>
-                    
-                    <div className="col-md-4">
-                        <h2>Secretary General</h2>
-                        <center>
-                            <div>
-                            <input type="radio" id="6" name="Secretary General" value="6"></input>
-                                <label for="7">Jane Doe</label><br></br>
-                                <input type="radio" id="8" name="Secretary General" value="8"></input>
-                                <label for="8">John Doe</label><br></br>
-                                <input type="radio" id="9" name="Secretary General" value="9"></input>
-                                <label for="9">Stephen</label><br></br>
-                            </div>
-                        </center>
-                        <Card/>
+                        <Card name1="Jane" name2="Martha" name3="Maria" title="Vice President"/>
                     </div>
                     <div className="col-md-4">
-                        <h2>Deputy S.General</h2>
-                        <center>
-                            <div>
-                                <input type="radio" id="jane_doe" name="Deputy Secretary General" value="jane_doe"></input>
-                                <label for="html">Jane Doe</label><br></br>
-                                <input type="radio" id="jane_doe" name="Deputy Secretary General" value="john_doe"></input>
-                                <label for="css">John Doe</label><br></br>
-                                <input type="radio" id="stephen" name="Deputy Secretary General" value="Stephen"></input>
-                                <label for="javascript">Stephen</label><br></br>
-                            </div>
-                        </center>
-                        <Card/>
-                    </div>
-                    <br></br>
-                    <div className="col-md-4">
-                        <h2>Treasurer</h2>
-                        <center>
-                            <div>
-                                <input type="radio" id="jane_doe" name="Treasurer" value="jane_doe"></input>
-                                <label for="html">Jane Doe</label><br></br>
-                                <input type="radio" id="jane_doe" name="Treasurer" value="john_doe"></input>
-                                <label for="css">John Doe</label><br></br>
-                                <input type="radio" id="stephen" name="Treasurer" value="Stephen"></input>
-                                <label for="javascript">Stephen</label><br></br>
-                            </div>
-                        </center>
-                        <Card/>
+                        <Card name1="Brenda" name2="Caesar" name3="Peter" title="Secretary General"/>
                     </div>
                     <div className="col-md-4">
-                        <h2>Spokesperson</h2>
-                        <center>
-                            <div>
-                                <input type="radio" id="jane_doe" name="Spokesperson" value="jane_doe"></input>
-                                <label for="html">Jane Doe</label><br></br>
-                                <input type="radio" id="jane_doe" name="Spokesperson" value="john_doe"></input>
-                                <label for="css">John Doe</label><br></br>
-                                <input type="radio" id="stephen" name="Spokesperson" value="Stephen"></input>
-                                <label for="javascript">Stephen</label><br></br>
-                            </div>
-                        </center>
-                        <Card/>
+                        <Card name1="Noah" name2="Oliver" name3="Jean" title="Deputy Secretary General"/> 
+                    </div>
+                    <div className="col-md-4">
+                        <Card name1="Liamson" name2="Joshua" name3="Tchalla" title=" Council Treasurer"/> 
+                    </div>
+                    <div className="col-md-4">
+                        <Card name1="Alice" name2="Nzisa" name3="Peter" title=" Council Spokesperson"/> 
                     </div>
                 </div>
-                <hr></hr>
-                <center><input type="submit" value="Submit"></input></center>
-                </form>    
             </div> 
         );
     }
