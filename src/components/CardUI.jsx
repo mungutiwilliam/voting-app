b25202f4e7840f36df2e49c78b901fab64b1686a
William <57761979+mungutiwilliam@users.noreply.github.com>
2022-03-04 19:48:53 +0300

    Updated Ui

--- src/components/CardUI.jsx
+++ src/components/CardUI.jsx
@@ -1,11 +1,40 @@
 import React from "react";
-import ps4 from '../assets/ps4.jpg'
+import './card-style.css';
+
+function countvotes(){
+    if(document.getElementById('candidate').checked) {   
+        var selectedValue = document.getElementById('candidate').value;  
+        alert("You have voted for: " + selectedValue);    
+ }  
+
+
+}
+
+
 
 const Card = (props) =>{
 
     return(
         <div className="card text-center">
+           <center>
+                <div className="list_names">   
+                                <input type="radio" id="candidate" name="Chairperson" value ={props.name1}></input>
+                                <label for="jane_doe">{props.name1}</label><br></br>
+                                <input type="radio" id="candidate" name="Chairperson" value={props.name2}></input>
+                                <label for="john_doe">{props.name2}</label><br></br>
+                                <input type="radio" id="candidate" name="Chairperson" value={props.name3}></input>
+                                <label for="stephen">{props.name3}</label><br></br>              
+                </div>
+            </center>
+            <div className="card-body text-dark">
+                <h4 className="card-title">{props.title}</h4>
+                <p className="card-text text-danger">
+                    Please Select One Candidate
+                </p>
+                <button className="btn btn-success">Submit vote</button>
+
             <div className="overflow">
+                
             </div>
             <div className="card-body text-dark">
                 <h4 className="card-title">Pick who you want to vote for.</h4>
@@ -13,7 +42,7 @@ const Card = (props) =>{
                 </p>
             </div>
         </div>
-
+</div>
     );
 }
 
