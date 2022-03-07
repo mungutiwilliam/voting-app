59b8d1c9aa76b3e2fb391c0b37a79c1190f09d08
William <57761979+mungutiwilliam@users.noreply.github.com>
2022-03-07 12:36:31 +0300

    UI updated

--- src/components/CardUI.jsx
+++ src/components/CardUI.jsx
@@ -1,41 +1,37 @@
 import React from "react";
 import './card-style.css';
 
-const handlechange = event =>{
-    const target = event.target
-    const name = target.name
-    const value = target.value
-    alert(`${name} ${value}`)
-}
-
-
 
 const Card = (props) =>{
 
+    
+
     return(
         <div className="bg-dark card text-center">
-           <center>
-                <div className="list_names">   
-                                <input on change ={handlechange}type="radio" id="candidate" name="Chairperson" value ={props.nname1}></input>
-                                <label for="jane_doe">{props.nname1}</label><br></br>
-                                <input type="radio" id="candidate2" name="Chairperson" value={props.nname2}></input>
-                                <label for="john_doe">{props.nname2}</label><br></br>
-                                <input type="radio" id="candidate3" name="Chairperson" value={props.nname3}></input>
-                                <label for="stephen">{props.nname3}</label><br></br>              
+            <form action="/action_page.php">
+                <center>
+                    <div className="list_names">   
+                                    <input type="radio" id="candidate" name ={props.title} value ={props.nname1}></input>
+                                    <label for="jane_doe">{props.nname1}</label><br></br>
+                                    <input type="radio" id="candidate2" name={props.title} value={props.nname2}></input>
+                                    <label for="john_doe">{props.nname2}</label><br></br>
+                                    <input type="radio" id="candidate3" name={props.title} value={props.nname3}></input>
+                                    <label for="stephen">{props.nname3}</label><br></br>
+                                    <hr></hr>
+                                    <div>
+                                         <button value="submit" type="submit"className="btn btn-success">Submit vote</button>
+                                    </div> 
+                                                
+                    </div>
+                </center>
+
+                <div className="card-body text-white">
+                    <h4 className="card-title">{props.title}</h4>
+                    <p className="card-text text-danger">
+                        Please Select One Candidate
+                    </p>      
                 </div>
-            </center>
-            <div className="card-body text-white">
-                <h4 className="card-title">{props.title}</h4>
-                <p className="card-text text-danger">
-                    Please Select One Candidate
-                </p>
-                <button className="btn btn-success">Submit vote</button>      
-            </div>
-            <div className="card-body text-white">
-                <h4 className="card-title">Pick who to vote for</h4>
-                <p className="card-text text-secondary">
-                </p>
-            </div>
+            </form>   
         </div>
     );
 }
